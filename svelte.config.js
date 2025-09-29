924633bc18cc6137d6ece706f429a183f68f8a5d
CorianiAmedeo <amedeocoriani@gmail.com>
2025-09-29 22:14:32 +0200

    Added initial files. Made Deploy script work with noJekyll and corrected errors.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  kit: {
+    adapter: adapter({
+      fallback: '404.html' // SPA fallback
+    }),
+    paths: {
+      base: ''
+    },
+    prerender: {
+      entries: ['*'] // pre-render all pages
+    }
+  }
+};
 
 export default config;
