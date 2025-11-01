003ff711c171ee6de827b96f05eb4b5164f9c215
CorianiAmedeo <amedeocoriani@gmail.com>
2025-11-01 22:45:38 +0100

    Fresh start.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-    import NetworkGraph from "$lib/components/NetworkGraph.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+Test
 
-<NetworkGraph/>
+<style>
+</style>
